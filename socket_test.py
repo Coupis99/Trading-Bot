b1408fda68ac983367535b1f30f7295742c685e9
Dominik Čoupek <dominik.coupek@gmail.com>
2022-08-01 00:05:18 +0200

    write to a file, placing orders, fixing position size

--- socket_test.py
+++ socket_test.py
@@ -1,6 +1,7 @@
 #imports
 from binance.client import Client
 from binance import ThreadedWebsocketManager
+from binance.exceptions import BinanceAPIException, BinanceOrderException
 from datetime import datetime, timedelta
 import pandas as pd
 from time import sleep
@@ -8,20 +9,12 @@ import json
 import requests
 
 symbol = "BTCUSDT"
+
 #binance api config - when publishing to github replace the keys with env variables
 api_key = "46332cbdcb7721395928c33e952fd7e801f6ed26b5838a5cd02143465b9004c7"
 api_secret = "cfea28c13b62df5277e529618a95a98c7b21215670475b8a794e1efdd16478d4"
 client = Client(api_key, api_secret, testnet=True)
 client.API_URL = "https://testnet.binancefuture.com/fapi"
 
-print(float(client.futures_account_balance()[1]["balance"]))
 
-# defining key/request url
-key=f"https://testnet.binancefuture.com/fapi/v1/ticker/24hr?symbol={symbol}"
-  
-# requesting data from url
-data = requests.get(key)  
-data = data.json()
-print(data["lastPrice"])
-
-
+print(len(client.futures_get_open_orders()))
--- trading_bot.py
+++ trading_bot.py
@@ -1,6 +1,7 @@
 #imports
 from binance.client import Client
 from binance import ThreadedWebsocketManager
+from binance.exceptions import BinanceAPIException, BinanceOrderException
 from datetime import datetime, timedelta
 import pandas as pd
 from time import sleep
@@ -18,6 +19,14 @@ client.API_URL = "https://testnet.binancefuture.com/fapi"
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
+
+#get current last price of given symbol
+def get_cur_last_price(smb):
+    key = f"https://testnet.binancefuture.com/fapi/v1/ticker/24hr?symbol={smb}"
+    data = requests.get(key)  
+    data = data.json()
+    res = float(data["lastPrice"])
+    return res
 
 #converting data into string separated with | and ||
 def conv_to_string(data):
@@ -40,20 +49,28 @@ def conv_to_string(data):
 def mm(close_t1, p_high, p_low):
     RRR = 2
     HH = 2
-    res = ""
+    res = {}
     size = 0
-    if abs(close_t1 - p_high) > (HH * abs(close_t1 - p_low)):
-        order_type = "long"
+    if (abs(close_t1 - p_high) > (HH * abs(close_t1 - p_low))) and (abs(p_high - p_low) > 80):
+        order_type = "BUY"
         sl = p_low
         tp = close_t1 + (2 * abs(close_t1 - p_low))
         size = round(pos_size(tp, close_t1), 2)
-        res = str(datetime.now())+ "|" + str(order_type) + "|" + str(size) + "|" + str(sl) + "|" + str(tp)
-    elif (HH * abs(close_t1 - p_high) < abs(close_t1 - p_low)):
-        order_type = "short"
+        res["DateTime"] = str(datetime.now())
+        res["OrderType"] = order_type
+        res["StopLoss"] = round(sl, 2)
+        res["TakeProfit"] = round(tp, 2)
+        res["PositionSize"] = size
+    elif ((HH * abs(close_t1 - p_high) < abs(close_t1 - p_low))) and (abs(p_high - p_low) > 80):
+        order_type = "SELL"
         sl = p_high
         tp = close_t1 - (2 * abs(close_t1 - p_high))
         size = round(pos_size(tp, close_t1), 2)
-        res = str(datetime.now()) + "|" + str(order_type) + "|" + str(size) + "|" + str(sl) + "|" + str(tp)
+        res["DateTime"] = str(datetime.now())
+        res["OrderType"] = order_type
+        res["StopLoss"] = round(sl, 2)
+        res["TakeProfit"] = round(tp, 2)
+        res["PositionSize"] = size
     else:
         print("není to ani jedno")
     return res
@@ -63,11 +80,68 @@ def pos_size(tp, close_t1):
     bal = float(client.futures_account_balance()[1]["balance"])
     reward = bal * 0.1
     pos_size = (reward * close_t1) / (abs(close_t1 - tp))
-    key = f"https://testnet.binancefuture.com/fapi/v1/ticker/24hr?symbol={symbol}"
-    data = requests.get(key)  
-    data = data.json()
-    cur_price = float(data["lastPrice"])
+    cur_price = get_cur_last_price(symbol)
+    if pos_size > 20 * bal:
+        pos_size = 18 * bal
     return float(pos_size) / float(cur_price)
+
+def place_order(type, sl, tp, size):
+    if type == "BUY":
+        try:
+            buy_market = client.futures_create_order(
+                symbol=symbol,
+                side='BUY',
+                type='MARKET',
+                quantity=size)
+            tp_sell_market = client.futures_create_order(
+                symbol = symbol,
+                side = "SELL",
+                type='TAKE_PROFIT_MARKET',
+                stopPrice=tp, 
+                closePosition=True, 
+                timeInForce='GTE_GTC')
+            sl_sell_market = client.futures_create_order(
+                symbol = symbol,
+                side = "SELL",
+                type='STOP_MARKET',
+                stopPrice=sl, 
+                closePosition=True, 
+                timeInForce='GTE_GTC')
+            print("order filled")
+        except BinanceAPIException as e:
+            #error handling goes here
+            print(e)
+        except BinanceOrderException as e:
+            #error handling goes here
+            print(e)
+    else:
+        try:
+            sell_market = client.futures_create_order(
+                symbol=symbol,
+                side='SELL',
+                type='MARKET',
+                quantity=size)
+            tp_buy_market = client.futures_create_order(
+                symbol = symbol,
+                side = "BUY",
+                type='TAKE_PROFIT_MARKET',
+                stopPrice=tp, 
+                closePosition=True, 
+                timeInForce='GTE_GTC')
+            sl_buy_market = client.futures_create_order(
+                symbol = symbol,
+                side = "BUY",
+                type='STOP_MARKET',
+                stopPrice=sl, 
+                closePosition=True, 
+                timeInForce='GTE_GTC')
+            print("order filled")
+        except BinanceAPIException as e:
+            #error handling goes here
+            print(e)
+        except BinanceOrderException as e:
+            #error handling goes here
+            print(e)
 
 #focusing only on BTCUSDT
 symbol = 'BTCUSDT'
@@ -76,10 +150,17 @@ symbol = 'BTCUSDT'
 bsm = ThreadedWebsocketManager()
 bsm.start()
 
-#test list
-test_lst = []
 #main cycle
 while True:
+
+    #wait until all positions are closed
+    while len(client.futures_get_open_orders()) > 0:
+        #do nothing
+        print(datetime.now())
+        print("cekam na ukonceni vsech pozic")
+        sleep(29)
+
+    print("vsechny pozice ukonceny")
 
     #wait until xx:00 or xx:30
     while (int(datetime.now().strftime("%M")) != 30) and ((int(datetime.now().strftime("%M")) != 0)):
@@ -93,9 +174,6 @@ while True:
     #finding datetime 1000 half hours back from now
     dt = datetime.now() - timedelta(hours = 500.5) 
     timestamp = int(datetime.timestamp(dt)) * 1000
-
-    print(dt)
-    print(timestamp)
 
     #request historical candle (or klines) data
     bars = client.futures_historical_klines(symbol, '30m', timestamp, limit=500)
@@ -126,12 +204,8 @@ while True:
         if pred_val_str != "":
             print("response arrived")
     
-    print(pred_val_str)
-
     #convert response from bytes to string 
     pred_val_str = pred_val_str.decode()
-
-    print(pred_val_str)
 
     #convert pred_val_str into a list with predicted values
     pred_val_lst = pred_val_str.split("|")
@@ -145,15 +219,20 @@ while True:
     close_t1 = float(df.loc[len(df) - 1, "Close"])
 
     order = mm(close_t1, pred_high, pred_low)
-    print(order)
-    if (order != ""):
-        test_lst.append(order)
 
-    print(test_lst)
-    print(close_t1)
+    #write to a file
+    if (order != {}):
+        print(order)
+        place_order(order["OrderType"], order["StopLoss"], order["TakeProfit"], order["PositionSize"])
+        print("zapisuji do souboru")
+        js = json.dumps(order, indent=4)
+        f = open("trade_log.txt", "a")
+        f.write("\n")
+        f.write(js)
+        f.close()
+    
     print("sleeping")
     sleep(60)
     
-
 bsm.stop()  
   
